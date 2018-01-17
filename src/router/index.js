782702d6803b706d51af5d86a7b8cdb804bbf793
zcw <1060813873@qq.com>
2018-01-17 16:05:16 +0800

    修改目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Cart from '@/components/Cart.vue'
 import ShoppingCart from '@/page/shoppingCart.vue'
 import ChooseStores from '@/page/home/ChooseStores.vue'
 import goodList from '@/components/goodList.vue'
-import checkstand from '@/page/checkstand.vue'
+import checkstand from '@/page/shoppingCart/checkstand.vue'
 Vue.use(Router)
 
 export default new Router({
